f809e403f262736d971ccac294080c71a3ecbc3b
Aponia <brianmv@uci.edu>
2023-08-25 17:16:44 -0700

    feat: ✨ initial commit 🎉

---
+++ documentation/astro.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'Svelte.Kit Docs',
+      social: {
+        github: 'https://github.com/ap0nia/svelte.kit',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            {
+              label: 'Example Guide',
+              link: '/guides/example/',
+            },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: {
+            directory: 'reference',
+          },
+        },
+      ],
+    }),
+  ],
+
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+    },
+  },
+})
