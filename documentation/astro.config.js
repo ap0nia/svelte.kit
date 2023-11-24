1a1588ef00cbc409fe8c2562366e4b87f095507a
Aponia <brianmv@uci.edu>
2023-11-24 03:11:22 -0800

    fix: 🐛 correct site and base path in config for local and prod

--- documentation/astro.config.js
+++ documentation/astro.config.js
@@ -1,30 +1,52 @@
-import { defineConfig } from 'astro/config'
+// @ts-check
+
 import starlight from '@astrojs/starlight'
+import ci from 'ci-info'
 
-export default defineConfig({
-  site: 'https://github.com/ap0nia/svelte.kit',
-  base: '/svelte.kit',
-  integrations: [
-    starlight({
-      title: 'Svelte.Kit',
-      social: {
-        github: 'https://github.com/ap0nia/svelte.kit',
-      },
-      sidebar: [
-        {
-          label: 'Packages',
-          autogenerate: {
-            directory: 'packages',
-          },
+import { repository } from './package.json'
+const repositoryName = repository.url.split('/').pop() ?? ''
+
+/**
+ * @returns {import('astro/config').AstroUserConfig}
+ */
+function defineConfig() {
+  /**
+   * @type {import('astro/config').AstroUserConfig}
+   */
+  const config = {
+    integrations: [
+      starlight({
+        title: 'Svelte.Kit',
+        social: {
+          github: 'https://github.com/ap0nia/svelte.kit',
         },
-      ],
-    }),
-  ],
+        sidebar: [
+          {
+            label: 'Packages',
+            autogenerate: {
+              directory: 'packages',
+            },
+          },
+        ],
+      }),
+    ],
 
-  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/sharp',
+    // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+    image: {
+      service: {
+        entrypoint: 'astro/assets/services/sharp',
+      },
     },
-  },
-})
+  }
+
+  if (ci.isCI) {
+    config.site = repository.url
+    config.base = `/${repositoryName}`
+  }
+
+  return config
+}
+
+const config = defineConfig()
+
+export default config
