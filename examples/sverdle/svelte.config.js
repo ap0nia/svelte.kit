682be5e50020a4089c2290e6efed0703f7738e52
Aponia <brianmv@uci.edu>
2024-03-25 18:05:03 -0700

    fix: 🐛 remove prisma from sverdle svelte config

--- examples/sverdle/svelte.config.js
+++ examples/sverdle/svelte.config.js
@@ -1,18 +1,7 @@
 // @ts-check
 
-import path from 'node:path';
 import adapter from '@svelte.kit/adapter-aws';
-import prisma from '@svelte.kit/adapter-aws/recipes/prisma';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
-const projectRoot = process.cwd();
-const engine = path.join(
-	projectRoot,
-	'node_modules',
-	'prisma',
-	'libquery_engine-rhel-openssl-1.0.x.so.node'
-);
-const schema = path.join(projectRoot, 'prisma', 'schema.prisma');
 
 /**
  * @type{import('@sveltejs/kit').Config}
@@ -20,9 +9,7 @@ const schema = path.join(projectRoot, 'prisma', 'schema.prisma');
 const config = {
 	preprocess: [vitePreprocess()],
 	kit: {
-		adapter: adapter({
-			lambdaUpload: prisma({ engine, schema })
-		})
+		adapter: adapter()
 	}
 };
 
